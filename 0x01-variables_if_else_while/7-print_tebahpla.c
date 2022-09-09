93a61d58cd60d6bd04623399d63234904fc4a479
racious555 <dorcasgracious@gmail.com>
2022-09-09 11:56:18 -0700

    My putchar script

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+/**
+ * main-prints the lowercase alphabetin reverse
+ *
+ * Return:0-success
+ */
+int main(void)
+{
+	int i;
+	for(i=122;i>96;i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
