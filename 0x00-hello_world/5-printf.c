239fdea74363cce9a68b27b1a0f27e74efd073a9
racious555 <dorcasgracious@gmail.com>
2022-09-09 20:36:33 -0700

    My printf function script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,8 @@
  * Return:0 after succesful printing.
  * return: Always 0.
  */
- * int main(void)
- * {
- * printf("with proper grammar, but the outcome is a piece of art");
- *
- * return (0);
- * }
+*int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
