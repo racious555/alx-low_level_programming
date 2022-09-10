eea52f33f5a2319fd69092aaf859b11541a56b5a
racious555 <dorcasgracious@gmail.com>
2022-09-09 20:26:12 -0700

    My printf script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Prints "with proper grammar, but the outcome is a piece of art"
- * Return:0
+ * Return:0 after succesful printing.
  * return: Always 0.
- *
+ */
  * int main(void)
  * {
- * printf("with proper grammar, but the outcome is a piece of art"\n");
+ * printf("with proper grammar, but the outcome is a piece of art");
  *
  * return (0);
  * }
