7351cddbc6af8b1e7a9323e9daf4b32d01ec69b2
racious555 <dorcasgracious@gmail.com>
2022-09-09 19:32:22 -0700

    My put function script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-/*
- * main - Prints "Programming is like building a multilingual 
- *  puzzle, followed by a new line.
- *   Return: Always 0.
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle"
+ *
+ * return Always 0.
  */
-
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
