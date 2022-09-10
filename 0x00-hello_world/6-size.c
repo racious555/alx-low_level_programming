fca25649c185bd8484892a9d612529a0d34f91b9
racious555 <dorcasgracious@gmail.com>
2022-09-09 21:09:07 -0700

    My size compiler script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various type
+ * main - prints the size of various types
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
@@ -10,5 +10,5 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));		printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return (0)
+	return (0);
 }
