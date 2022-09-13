46d7d7648f093844d8eec5c831ff15211cf10b7f
racious555 <dorcasgracious@gmail.com>
2022-09-12 17:50:39 -0700

    My Dorcas signs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
-* main - Entry point
-*
+* main - Entry point for n
+* Returns and prints it
 * Return: Always 0 (Success)
 */
 int main(void)
@@ -13,13 +13,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-	
 	return (0);
 }
