2e005519c9d97a1ebd12ab8736243a1cbc72ca5e
racious555 <dorcasgracious@gmail.com>
2022-09-12 18:06:41 -0700

    My print letter commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in small letters,
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
