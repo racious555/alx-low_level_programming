eaf40cae487b59618b80b481b0820115891e9543
racious555 <dorcasgracious@gmail.com>
2022-09-20 09:21:11 -0700

    My array commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,5 @@
 /**
-* swap_int - swaps the values of two integers
-* 		using two input parameters
+* swap_int - swaps the values of two integers using two input parameters
 *
 * @a: input parameter 1
 * @b: input parameter 2
