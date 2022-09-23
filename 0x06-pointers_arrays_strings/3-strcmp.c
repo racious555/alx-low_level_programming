0547c9c59102a8ac2e409cd920544de49e016986
racious555 <dorcasgracious@gmail.com>
2022-09-23 04:59:03 -0700

    My array 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,11 +6,11 @@
 *
 * Return: 0 if s1 and s2 are equal
 * 	negative integer if the stopping character
-* 	in @s1 was less than the stopping
-* 	character in @s2
+* 		in @s1 was less than the stopping
+* 		character in @s2
 * 	positive integer if the stopping character
-* 	in @s1 was greater than the stopping
-* 	character in @s2
+* 		in @s1 was greater than the stopping
+* 		character in @s2
 */
 
 int _strcmp(char *s1, char *s2)
@@ -20,19 +20,19 @@ int _strcmp(char *s1, char *s2)
 	/**
 	 * iterate through src and
 	 * compare it with dest
-	 */
+	*/
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		/**
 		 * if they differ by a single character
 		 * don't iterate further
-		 */
+		*/
 		if (s1[i] != s2[i])
 		{
-			/*
+			/**
 			 * return the difference between
 			 * the two characters
-			 */
+			*/
 			r = s1[i] - s2[i];
 			break;
 		}
