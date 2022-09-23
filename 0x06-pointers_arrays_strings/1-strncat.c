b22539e15e944c1d1b2c8e8412c3bb902ae29253
racious555 <dorcasgracious@gmail.com>
2022-09-23 03:01:56 -0700

    My arrays 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,32 @@
-#include "main.h"
-
 /**
-* _strncat - appends src to the dest string
-* @dest: string to append by src
-* @src: string to append to dest
-* @n: largest number of bytes to append
-* Return: address of dest
+*
+* _strncat - a function that concatenates two strings.
+*
+* @dest: pointer to destination input
+* @src: pointer to source input
+* @n: most number of bytes from @src
+*
+* Return: @dest
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int c, d;
-	c = d = 0;
-	 while (*(dest + c))
-		 c++;
-	 while (d < n && *(src + d))
-	 {
-		 *(dest + c) = *(src + d);
-		 c++;
-		 d++;
-	 }
-	 if (d < n)
-		 *(dest + c) = *(src + d);
-	
-	 return (dest);
+	int c, i;
+
+	c = 0;
+
+	/*find size of dest array*/
+	while (dest[c])
+		c++;
+
+	/**
+	 * src does not need to be null terminated
+	 * if it contains n or more bytes
+	 */
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	/*null terminate dest*/
+	dest[c + i] = '\0';
+
+	return (dest);
 }
