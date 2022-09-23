d90e9bdc38e9fdbde481dd0d3a1e13262bf9bbd4
racious555 <dorcasgracious@gmail.com>
2022-09-23 04:27:11 -0700

    My array 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 /**
-*
 * _strncat - a function that concatenates two strings.
 *
 * @dest: pointer to destination input
@@ -20,8 +19,8 @@ char *_strncat(char *dest, char *src, int n)
 		c++;
 
 	/**
-	 * src does not need to be null terminated
-	 * if it contains n or more bytes
+	  * src does not need to be null terminated
+	  * if it contains n or more bytes
 	 */
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[c + i] = src[i];
