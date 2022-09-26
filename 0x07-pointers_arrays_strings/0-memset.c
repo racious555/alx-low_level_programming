9fbe1d066f4e38ab140406361a970331ce448fcf
racious555 <dorcasgracious@gmail.com>
2022-09-26 15:55:19 -0700

    My pointer of pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-* _memset - fills memory with a constant byte
+* *_memset - fills memory with a constant byte
 * @s: pointer to put the constant
 * @b: constant
 * @n: max bytes to use
@@ -27,17 +27,4 @@ void simple_print_buffer(char *buffer, unsigned int size)
 		i++;
 	}
 	printf("\n");
-}
-/**
-* main - check the code
-* Return: Always 0.
-*/
-int main(void)
-{
-	char buffer[98] = {0x00};
-	simple_print_buffer(buffer, 98);
-	_memset(buffer, 0x01, 95);
-	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);
-	return (0);
 }
