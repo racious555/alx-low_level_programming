c0c9c34024be4246d9baa0df9ea7e8e00452796e
racious555 <dorcasgracious@gmail.com>
2022-09-26 15:01:56 -0700

    My pointer pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,13 +8,36 @@
 * @n: max bytes to use
 * Return: s
 */
-char *_memset(char *s, char b, unsigned int n)
+void simple_print_buffer(char *buffer, unsigned int size)
 {
 	unsigned int i;
 
-	for (i = 0; n > 0 ; i++, n--)
+	i = 0;
+	while (i < size)
 	{
-		s[i] = b;
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
 	}
-	return (s);
+	printf("\n");
+}
+/**
+* main - check the code
+* Return: Always 0.
+*/
+int main(void)
+{
+	char buffer[98] = {0x00};
+	simple_print_buffer(buffer, 98);
+	_memset(buffer, 0x01, 95);
+	printf("-------------------------------------------------\n");
+	simple_print_buffer(buffer, 98);
+	return (0);
 }
